46c403f8ca91304f75edabcdc03edd8919abd95d
Joanna Yuhas <joannayuhas@gmail.com>
2019-03-21 21:51:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   if array.size == 2
     array.join(" and ")
   elsif array.size == 3
-  array.split_between(array.last)
+  array.split_between(array.last).to_a
   else 
     array.join(", ")
   end 
